9cb7a8b4e040192fa857901d2037bc6877a1d1bb
Shengrui Cen <cenrui.93@gmail.com>
2017-06-10 14:18:48 -0700

    start working on multilayer perceptron

---
+++ multilayer_perceptron/multilayer_perceptron_classifier.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class MLP:
+    def __init__(self):
+    # TODO implement
+    def get_weight_bound(self):
+    # TODO implement
+
+    def fit(self, X, y, shuffle):
+
+    def compute_loss(self, X, y):
+
+    def forward(self, X):
+
+    def backward_prop(self, X, y):
+
+    def predict(self, X):
+
+    def score(self, X, y):
+
+
+
